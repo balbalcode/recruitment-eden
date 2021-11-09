3790e08f7e9380e9db12eef6337d778b306bad08
balbalcode <iqbalmaulana027@gmail.com>
2021-11-09 09:20:33 +0700

    chore: initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>EDEN Tech Test</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+	<section class="container my-5 rounded border">
+		<?php
+		$data = array(
+			0 => array(
+				'id' => 1,
+				'name' => 'Foo', 'image' => array( 'url' => '/pub/foo.jpg')
+			),
+			1 => array(
+				'id' => 2,
+				'name' => 'Bar', 'image' => array('url' => '/pub/bar.jpg' )
+			),
+			2 => array(
+				'id' => 3,
+				'name' => 'Foo Bar', 'image' => array( 'url' => '/pub/foobar.jpg')
+			)
+		);
+
+		foreach ($data as $key => $value) {
+			echo$
+		}
+		?>
+
+
+	</section>
+</body>
+<script type="text/javascript">
+	var data = [
+	{type:"Foo", year:1995},
+	{type:"Bar", year:1993},
+	{type:"Foobar", year : 2020}
+	];
+
+	// this is only sort a - b so we can use array.sort(funct => a-b)
+	let sortedData = data.sort((prev, next) =>{
+		return (next - prev)
+	})
+	console.log(data, "DATA")
+	console.log(sortedData , "SORTED DATA")
+</script>
+</html>
